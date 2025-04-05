84fef863b1c0ded3d9042d665b86bf53619a6f74
mwsohn <mwsohn@gmail.com>
2025-04-04 22:40:02 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
-using CategoricalArrays, DataFrames, GLM, RDatasets
+using CategoricalArrays, DataFrames, GLM, RDatasets, DominanceAnalysis, Stella
 
 mtcars = dataset("datasets","mtcars")
+renvars!(mtcars)
 
 
 @testset "Linear Regression without sets" begin
@@ -8,15 +9,15 @@ mtcars = dataset("datasets","mtcars")
     dom1 = dominance(mtcars, :mpg, [:am, :cyl, :carb])
 
     # general dominance compared to the values obtained from domir.R
-    @test isapprox(dom1.fit_overall, 0.8113023)
-    @test isapprox(dom1.domstat[1,1], 0.2156848) # am
-    @test isapprox(dom1.domstat[2,1], 0.4173094) # cyl
-    @test isapprox(dom1.domstat[3,1], 0.1783081) # carb
+    @test isapprox(dom1.fit_overall, 0.8113023, rtol = 0.0001)
+    @test isapprox(dom1.domstat[1, 1], 0.2156848, rtol=0.0001) # am
+    @test isapprox(dom1.domstat[2, 1], 0.4173094, rtol=0.0001) # cyl
+    @test isapprox(dom1.domstat[3, 1], 0.1783081, rtol=0.0001) # carb
 
     # conditional dominance
-    @test isapprox(dom1.constat[1, 3], 0.07076149) # am at 3
-    @test isapprox(dom1.constat[2, 3], 0.10762967) # cyl at 3
-    @test isapprox(dom1.constat[3, 3], 0.05228872) # carb at 3
+    @test isapprox(dom1.constat[1, 3], 0.07076149, rtol=0.0001) # am at 3
+    @test isapprox(dom1.constat[2, 3], 0.10762967, rtol=0.0001) # cyl at 3
+    @test isapprox(dom1.constat[3, 3], 0.05228872, rtol = 0.0001) # carb at 3
 
 end
 
@@ -24,17 +25,17 @@ end
 
     dom2 = dominance(mtcars, :mpg, [:am, :cyl, (:carb, :wt)])
 
-    @test isapprox(dom2.fit_overall, 0.8502) # overall fit statistic
+    @test isapprox(dom2.fit_overall, 0.8502, rtol=0.0001) # overall fit statistic
 
     # general dominance compared to the values from Stata domin
-    @test isapprox(dom2.domstat[1, 1], 0.1307) # am
-    @test isapprox(dom2.domstat[2, 1], 0.3308) # cyl
-    @test isapprox(dom2.domstat[3, 1], 0.3887) # Set 1 (carb wt)
+    @test isapprox(dom2.domstat[1, 1], 0.1307, rtol=0.01) # am
+    @test isapprox(dom2.domstat[2, 1], 0.3308, rtol=0.01) # cyl
+    @test isapprox(dom2.domstat[3, 1], 0.3887, rtol=0.01) # Set 1 (carb wt)
 
     # conditional dominance compared to the values from Stata domin
-    @test isapprox(dom2.constat[1, 3], 0.0077) # am
-    @test isapprox(dom2.constat[2, 3], 0.0417) # cyl
-    @test isapprox(dom2.constat[3, 3], 0.0912) # Set 1 (carb wt)
+    @test isapprox(dom2.constat[1, 3], 0.0077, rtol=0.01) # am
+    @test isapprox(dom2.constat[2, 3], 0.0417, rtol=0.01) # cyl
+    @test isapprox(dom2.constat[3, 3], 0.0912, rtol=0.01) # Set 1 (carb wt)
 end
 
 
